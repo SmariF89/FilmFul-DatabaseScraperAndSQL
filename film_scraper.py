113f99a333aefaa5c099d67064347ce22e8567a6
clover89 <smarifreyr30@gmail.com>
2020-01-15 22:07:31 +0000

    Improved upon script structure.

--- film_scraper.py
+++ film_scraper.py
@@ -10,43 +10,90 @@
 
 from bs4 import BeautifulSoup
 from requests import get
-from modules.film_scraper_constants import IMDB_URL
+from modules.film_scraper_constants import  IMDB_URL,               \
+                                            ACTORS_SCRIPT_NAME,     \
+                                            DIRECTORS_SCRIPT_NAME,  \
+                                            MOVIES_SCRIPT_NAME,     \
+                                            ACTIONS_SCRIPT_NAME,    \
+                                            DIRECTIONS_SCRIPT_NAME, \
+                                            GENRES_SCRIPT_NAME
 
-# ===========================   Scraping functions  =========================== #
+# ===========================   Data gathering functions  =========================== #
 
-def scrape():
-    if  get_actors()     and \
-        get_directors()  and \
-        get_movies()     and \
-        get_actions()    and \
-        get_directions() and \
-        get_genres():
-        return True
-    return False
+actors = []
+directors = []
+movies = []
+actions = []
+directions = []
+genres = []
+
+def get_data():
+    return  get_actors()     and \
+            get_directors()  and \
+            get_movies()     and \
+            get_actions()    and \
+            get_directions() and \
+            get_genres()
 
 def get_actors():
-    writeToFile('', '')
+    actors = None
     return False
 
 def get_directors():
-    writeToFile('', '')
+    directors = None
     return False
 
 def get_movies():
-    writeToFile('', '')
+    movies = None
     return False
 
 def get_actions():
-    writeToFile('', '')
+    actions = None
     return False
 
 def get_directions():
-    writeToFile('', '')
+    directors = None
     return False
 
 def get_genres():
+    genres = None
+    return False
+
+# ===========================   SQL generation functions  =========================== #
+
+def generate_sql_population_scripts():
+    return  generate_actors_sql()     and \
+            generate_directors_sql()  and \
+            generate_movies_sql()     and \
+            generate_actions_sql()    and \
+            generate_directions_sql() and \
+            generate_genres_sql()
+
+def generate_actors_sql():
     writeToFile('', '')
     return False
+
+def generate_directors_sql():
+    writeToFile('', '')
+    return False
+
+def generate_movies_sql():
+    writeToFile('', '')
+    return False
+
+def generate_actions_sql():
+    writeToFile('', '')
+    return False
+
+def generate_directions_sql():
+    writeToFile('', '')
+    return False
+
+def generate_genres_sql():
+    writeToFile('', '')
+    return False
+
+# ===========================   Utility functions  =========================== #
 
 # TODO: Throw error if incorrect filename
 # Expects a list of strings which will be written to a file, newline separated.
@@ -57,8 +104,10 @@ def writeToFile(file, dataToWrite):
     f.write(sep.join(dataToWrite))
     f.close()
 
+# ===========================   Script root   =========================== #
+
 def main():
-    if scrape():
+    if get_data() and generate_sql_population_scripts():
         print("Scrape successful!")
         return 0
     print("Scrape failed!")
