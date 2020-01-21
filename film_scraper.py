c18ef1dc5d834f3e68ea0c7553a5829dfc1de2cb
clover89 <smarifreyr30@gmail.com>
2020-01-21 00:01:03 +0000

    All data mapping functions have been implemented except for get_poster()

--- film_scraper.py
+++ film_scraper.py
@@ -54,30 +54,45 @@ def accumulate_data(data):
         # get_actors(imdb_dic, movie_item, movie_title)
         # get_directors(imdb_dic, movie_item, movie_title)
         # get_genres(imdb_dic, movie_item, movie_title)
-        # get_poster(imdb_dic, movie_item, movie_title)             # TODO
+         get_poster(imdb_dic, movie_item, movie_title)             # TODO
         # get_description(imdb_dic, movie_item, movie_title)
         # get_duration(imdb_dic, movie_item, movie_title)
-        get_release_year(imdb_dic, movie_item, movie_title)
-        """get_rating_imdb(imdb_dic, movie_item, movie_title)
-        get_rating_metascore(imdb_dic, movie_item, movie_title)"""
+        # get_release_year(imdb_dic, movie_item, movie_title)
+        # get_rating_imdb(imdb_dic, movie_item, movie_title)
+        # get_rating_metascore(imdb_dic, movie_item, movie_title)
         # get_certificate(imdb_dic, movie_item, movie_title)
-        """get_gross(imdb_dic, movie_item, movie_title)
-        get_vote_count(imdb_dic, movie_item, movie_title)"""
+        # get_gross(imdb_dic, movie_item, movie_title)
+        # get_vote_count(imdb_dic, movie_item, movie_title)
 
     print(imdb_dic)
     return imdb_dic
 
 def get_title(dic, movie):
-    title = movie.find_next("div", class_="lister-item-content").find_next("h3").find_next("a").get_text()
+    title = movie                                       \
+        .find_next("div", class_="lister-item-content") \
+        .find_next("h3")                                \
+        .find_next("a")                                 \
+            .get_text()
+
     dic[title] = {}
     dic[title]["title"] = title
     return title
 
 def get_actors(dic, movie, title):
-    dic[title]["actors"] = [tag.get_text() for tag in movie.find_next("div", class_="lister-item-content").find_all("p")[2].find_next("span").find_next_siblings("a")]
+    dic[title]["actors"] = [tag.get_text() for tag in movie \
+        .find_next("div", class_="lister-item-content")     \
+        .find_all("p")[2]                                   \
+        .find_next("span")                                  \
+        .find_next_siblings("a")                            \
+    ]
 
 def get_directors(dic, movie, title):
-    directors = [tag.get_text() for tag in movie.find_next("div", class_="lister-item-content").find_all("p")[2].find_next("span").find_previous_siblings("a")]
+    directors = [tag.get_text() for tag in movie        \
+        .find_next("div", class_="lister-item-content") \
+        .find_all("p")[2]                               \
+        .find_next("span")                              \
+        .find_previous_siblings("a")
+    ]
     
     # If movie has more than one director, the scraper gets directors in reverse order. This line fixes the order.
     if len(directors) > 1:
@@ -85,7 +100,11 @@ def get_directors(dic, movie, title):
     dic[title]["directors"] = directors
 
 def get_genres(dic, movie, title):
-    genres_str = movie.find_next("div", class_="lister-item-content").find_next("p", class_="text-muted").find_next("span", class_="genre").get_text()
+    genres_str = movie                                  \
+        .find_next("div", class_="lister-item-content") \
+        .find_next("p", class_="text-muted")            \
+        .find_next("span", class_="genre")              \
+            .get_text()
     
     # The first entry always starts with a "\" which must be removed.
     genres_str = genres_str[1:]
@@ -107,33 +126,105 @@ def get_description(dic, movie, title):
     # The second p-tag within "lister-item-content" with the class "text-muted"
     # contains the description. It has unwanted newlines and whitespaces which
     # are stripped off.
-    dic[title]["description"] = movie.find_next("div", class_="lister-item-content").find_all("p", class_="text-muted")[1].get_text().strip("\n").strip(" ")
+    dic[title]["description"] = movie                   \
+        .find_next("div", class_="lister-item-content") \
+        .find_all("p", class_="text-muted")[1]          \
+            .get_text()                                 \
+                .strip("\n")                            \
+                .strip(" ")
 
 def get_duration(dic, movie, title):
     # IMDB's format is e.g. "89 min" but we are only interested in the number.
     # We want to keep the number as an integer in order to order the films by
     # duration.
-    dic[title]["duration"] = int(movie.find_next("div", class_="lister-item-content").find_next("p", class_="text-muted").find_next("span", class_="runtime").get_text().split(" ")[0])
+    dic[title]["duration"] = int(movie
+        .find_next("div", class_="lister-item-content") \
+        .find_next("p", class_="text-muted")            \
+        .find_next("span", class_="runtime")            \
+            .get_text()                                 \
+                .split(" ")[0]
+    )
 
-# TODO: Var kominn hingad. Vandi: Myndir eins og t.d. Coco hafa release_year sem "(I) (2017)". Taekla thessi case.
 def get_release_year(dic, movie, title):
-    # dic[title]["release_year"] = int(movie.find_next("div", class_="lister-item-content").find_next("h3", class_="lister-item-header").find_next("span", class_="lister-item-year text-muted unbold").get_text().strip("(").strip(")"))
-    dic[title]["release_year"] = movie.find_next("div", class_="lister-item-content").find_next("h3", class_="lister-item-header").find_next("span", class_="lister-item-year text-muted unbold").get_text()
+    year_string = movie                                                     \
+        .find_next("div", class_="lister-item-content")                     \
+        .find_next("h3", class_="lister-item-header")                       \
+        .find_next("span", class_="lister-item-year text-muted unbold")     \
+            .get_text()
+
+    # year_string sometimes contains untwanted extra characters.
+    if len(year_string) > 6:
+        dic[title]["release_year"] = int(                           \
+            year_string.split(" ")[1]                               \
+                .strip("(")                                         \
+                .strip(")")                                         \
+        )
+    else:
+        dic[title]["release_year"] = int(year_string                \
+            .strip("(")                                             \
+            .strip(")")                                             \
+        )
+    
 
 def get_rating_imdb(dic, movie, title):
-    return None
+    dic[title]["rating_imdb"] = float(movie                         \
+        .find_next("div", class_="lister-item-content")             \
+        .find_next("div", class_="ratings-bar")                     \
+        .find_next("div", class_="inline-block ratings-imdb-rating")\
+        .find_next("strong")                                        \
+            .get_text()                                             
+    )
+
 
 def get_rating_metascore(dic, movie, title):
-    return None
+    # Some films do not have metascore.
+    
+    metascore_string_tree_path = movie                          \
+        .find_next("div", class_="lister-item-content")         \
+        .find_next("div", class_="ratings-bar")                 \
+        .find(class_="inline-block ratings-metascore")
+
+    if metascore_string_tree_path:
+        dic[title]["rating_metascore"] = int(metascore_string_tree_path
+            .find_next("span").get_text().strip(" "))
+    else:
+        dic[title]["rating_metascore"] = None
 
 def get_certificate(dic, movie, title):
-    dic[title]["certificate"] = movie.find_next("div", class_="lister-item-content").find_next("p", class_="text-muted").find_next("span", class_="certificate").get_text()
+    dic[title]["certificate"] = movie                           \
+        .find_next("div", class_="lister-item-content")         \
+        .find_next("p", class_="text-muted")                    \
+        .find_next("span", class_="certificate")                \
+            .get_text()
 
 def get_gross(dic, movie, title):
-    return None
+    gross_string_tree_path = movie                              \
+        .find_next("div", class_="lister-item-content")         \
+        .find_next("p", class_="sort-num_votes-visible")        \
+        .find_all("span", class_="text-muted")
+
+    # If there are two span tags with class "text-muted", the gross data is available.
+    if len(gross_string_tree_path) == 2:
+        dic[title]["gross"] = float(gross_string_tree_path[1]   \
+            .next_sibling                                       \
+            .next_sibling                                       \
+                .get_text()                                     \
+                    .strip("$")                                 \
+                    .strip("M")
+        )
+    else:
+        dic[title]["gross"] = None
 
 def get_vote_count(dic, movie, title):
-    return None
+    dic[title]["vote_count"] = int(movie                        \
+        .find_next("div", class_="lister-item-content")         \
+        .find_next("p", class_="sort-num_votes-visible")        \
+        .find_next("span", class_="text-muted")                 \
+            .next_sibling                                       \
+            .next_sibling                                       \
+                .get_text()                                     \
+                    .replace(",", "")
+    )
 
 # ===========================   SQL generation functions  =========================== #
 
