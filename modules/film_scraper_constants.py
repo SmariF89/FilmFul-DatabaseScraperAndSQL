8dc0cd0856ec5d926e5c18b2dac89543c9f014b0
clover89 <smarifreyr30@gmail.com>
2020-01-24 00:24:09 +0000

    Forgot to commit the modules. Here they come.

--- modules/film_scraper_constants.py
+++ modules/film_scraper_constants.py
@@ -1,8 +1,10 @@
-IMDB_URLS = [ "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc",
+"""IMDB_URLS = [ "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc",
               "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc&start=51&ref_=adv_nxt",
               "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc&start=101&ref_=adv_nxt",
               "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc&start=151&ref_=adv_nxt",
-              "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc&start=201&ref_=adv_nxt"]
+              "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc&start=201&ref_=adv_nxt"]"""
+
+IMDB_URLS = [ "https://www.imdb.com/search/title/?groups=top_250&sort=alpha,asc"]
 
 ACTORS_SCRIPT_NAME = "FILL_ACTOR.sql"
 DIRECTORS_SCRIPT_NAME = "FILL_DIRECTOR.sql"
@@ -10,3 +12,10 @@ MOVIES_SCRIPT_NAME = "FILL_MOVIE.sql"
 ACTIONS_SCRIPT_NAME = "FILL_ACTION.sql"
 DIRECTIONS_SCRIPT_NAME = "FILL_DIRECTION.sql"
 GENRES_SCRIPT_NAME = "FILL_GENRE.sql"
+
+ACTOR_SCRIPT_START = """INSERT INTO actor(name) VALUES\n"""
+DIRECTOR_SCRIPT_START = """INSERT INTO director(name) VALUES\n"""
+ACTION_SCRIPT_START = """INSERT INTO action(actor_id, movie_id) VALUES\n"""
+DIRECTION_SCRIPT_START = """INSERT INTO direction(director_id, movie_id) VALUES\n"""
+GENRE_SCRIPT_START = """INSERT INTO genre(movie_id, genre) VALUES\n"""
+MOVIE_SCRIPT_START = """INSERT INTO movie(title, poster, description, duration, release_year, rating_imdb,\nrating_metascore, certificate, gross, vote_count) VALUES\n"""
